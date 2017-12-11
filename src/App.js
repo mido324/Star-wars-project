e2ba8d141e83ed4c986479a10696b81a0c7446a9
mido <mido.ghanim@gmail.com>
2017-12-10 18:02:13 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import List from './List';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      people: []
+
+    }
+    this.people = this.getChar.bind(this);
+  }
+  getChar() {
+    return axios.get('https://swapi.co/api/people').then((res) => {
+      console.log(res.data.results);
+      this.setState({people: res.data.results})
+    })
+  }
+  componentDidMount() {
+    this.getChar()
+  }
+  
+
+  render() {
+    const { people } = this.state;
+
+    return (
+      <div className="App">
+     
+
+     <List people={people}/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
